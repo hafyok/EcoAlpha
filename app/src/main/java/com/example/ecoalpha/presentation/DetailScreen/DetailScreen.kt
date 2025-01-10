19424b7ade207c12679e7feb42725df320958e57
Haggai Khafizov <hafyok5777@gmail.com>
2025-01-10 13:02:56 +0300

    Fix ui, almost full info about card

--- app/src/main/java/com/example/ecoalpha/presentation/DetailScreen/DetailScreen.kt
+++ app/src/main/java/com/example/ecoalpha/presentation/DetailScreen/DetailScreen.kt
@@ -1,11 +1,14 @@
 package com.example.ecoalpha.presentation.DetailScreen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,6 +17,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.ecoalpha.data.Entity.BinCardInfoEntity
+import com.example.ecoalpha.presentation.HomeScreen.CardInfoRow
 
 @Composable
 fun DetailScreen(
@@ -25,15 +30,42 @@ fun DetailScreen(
             .fillMaxSize()
             .padding(16.dp)
     ) {
+        item { Text(text = "Query history") }
         items(cards){
             card ->
-            BankCardInfo(label = "label", value = card.type.toString())
+            if (card.scheme != null) BankCardInfo(card)
+            else {
+                // TODO()
+            }
         }
     }
 }
 
 @Composable
-fun BankCardInfo(label: String, value: String) {
+fun BankCardInfo(cardInfo: BinCardInfoEntity){
+    Card(modifier = Modifier.padding(8.dp), elevation = 4.dp) {
+        Column {
+            BankCardInfoRow(label = "Scheme / Network", value = cardInfo.scheme.toString())
+            BankCardInfoRow(label = "Type", value = cardInfo.type.toString())
+            BankCardInfoRow(label = "Brand", value = cardInfo.brand.toString())
+            BankCardInfoRow(label = "Prepaid", value = cardInfo.prepaid.toString())
+            BankCardInfoRow(
+                label = "Card Number",
+                value = "Length: ${cardInfo.number?.length}\nLuhn: ${cardInfo.number?.luhn}"
+            )
+            BankCardInfoRow(label = "Country", value = cardInfo.country?.name.toString())
+            BankCardInfoRow(
+                label = "Bank",
+                value = cardInfo.bank?.name.toString()
+            )
+        }
+    }
+
+}
+
+
+@Composable
+fun BankCardInfoRow(label: String, value: String) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -41,12 +73,11 @@ fun BankCardInfo(label: String, value: String) {
     ) {
         Text(
             text = label,
-            //style = MaterialTheme.typography.body2,
             color = Color.Gray
         )
+        Spacer(modifier = Modifier.padding(horizontal = 8.dp))
         Text(
             text = value,
-            //style = MaterialTheme.typography.body1
         )
     }
 }
